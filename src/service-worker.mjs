1fff941f9168a20514922b764649dce12a1560d3
Jeff Posnick <jeffy@google.com>
2018-11-07 13:23:43 -0500

    Workbox v4.0.0-beta.0

--- src/service-worker.mjs
+++ src/service-worker.mjs
@@ -20,7 +20,7 @@ import * as urls from './lib/urls.mjs';
 import partials from './lib/partials.mjs';
 import routeMatchers from './lib/route-matchers.mjs';
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0-beta.0/workbox-sw.js');
 workbox.setConfig({
   debug: true,
 });
